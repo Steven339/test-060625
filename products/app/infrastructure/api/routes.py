5faec87534c80af5ddfe47234a0e162a45892cde
Brayan Correa <brayancorrea78@gmail.com>
2025-06-06 14:52:08 -0500

    create royte for create product

---
+++ products/app/infrastructure/api/routes.py
@@ -0,0 +1,25 @@
+from app.application.use_cases import create_product
+from app.domain.models import ProductCreate
+from app.infrastructure.db.session import SessionLocal
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.orm import Session
+
+router = APIRouter()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@router.post("/products", response_model=dict, status_code=status.HTTP_201_CREATED)
+def create_product(request: ProductCreate, db: Session = Depends(get_db)):
+    product = create_product(db, request)
+    return {
+        "data":{
+            "type": "products",
+            "id": product.id,
+            "attributes": product
+        }
+    }
