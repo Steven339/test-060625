212cfa0fda84ca1ce056f02edea1dd50413f2c86
Brayan Correa <brayancorrea78@gmail.com>
2025-06-06 15:16:47 -0500

    fix database url

--- products/app/infrastructure/db/session.py
+++ products/app/infrastructure/db/session.py
@@ -3,7 +3,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgres://postgres:postgres@localhost:5432/products")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@db:5432/products")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
