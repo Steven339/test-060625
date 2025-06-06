8b548223c45c398ccaa5795354af651da9a860ed
Brayan Correa <brayancorrea78@gmail.com>
2025-06-06 14:40:49 -0500

    added db session

---
+++ products/app/infrastructure/db/session.py
@@ -0,0 +1,10 @@
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = os.getenv("DATABASE_URL", "postgres://postgres:postgres@localhost:5432/products")
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
