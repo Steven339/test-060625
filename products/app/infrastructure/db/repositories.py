6382c67155412b7eba4c160d3378de3281c94092
Brayan Correa <brayancorrea78@gmail.com>
2025-06-06 14:41:31 -0500

    product repo created

---
+++ products/app/infrastructure/db/repositories.py
@@ -0,0 +1,17 @@
+from app.domain.models import Product
+from app.infrastructure.db.models import ProductDB
+from sqlalchemy.orm import Session
+
+
+class ProductRepository:
+    def __init__(self, db: Session):
+        self.db = db
+
+    def create(self, product: Product):
+        db_product = ProductDB(**product.dict())
+        self.db.add(db_product)
+        self.db.commit()
+        self.db.refresh(db_product)
+        return db_product
+
+        
