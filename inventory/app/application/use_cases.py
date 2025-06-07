53411e5b3d93bb521d7faf015f906153c4b127ce
Brayan Correa <brayancorrea78@gmail.com>
2025-06-07 13:55:50 -0500

    added tenacity for retries

--- inventory/app/application/use_cases.py
+++ inventory/app/application/use_cases.py
@@ -1,14 +1,29 @@
 import httpx
 from app.domain.models import Inventory
 from app.domain.repositories import AbstractRepository
+from tenacity import retry, retry_if_exception_type, stop_after_attempt, wait_fixed
+
+""" Inventory use cases """
+
+PRODUCTS_URL = "http://products:8000/products"
 
 
-def check_product(producto_id: int):
-    response = httpx.get(f"http://products:8000/products/{producto_id}")
-    if response.status_code != 200:
-        raise ValueError("Producto no existe")
+@retry(
+    stop=stop_after_attempt(3),
+    wait=wait_fixed(1),
+    retry=retry_if_exception_type(httpx.RequestError)
+)
+def check_product(product_id: int):
+    """ Check if product exists """
+    try:
+        response = httpx.get(f"{PRODUCTS_URL}/{product_id}", timeout=2.0)
+        if response.status_code != 200:
+            raise ValueError("Product not found")
+    except httpx.RequestError as e:
+        raise ValueError(f"Connection error: {e}")
 
 def get_inventory(repository: AbstractRepository, product_id: int) -> Inventory:
+    """ Get inventory by product id """
     check_product(product_id)
     inventory = repository.get(product_id)
     if inventory:
@@ -16,6 +31,7 @@ def get_inventory(repository: AbstractRepository, product_id: int) -> Inventory:
     return None
 
 def update_inventory(repository: AbstractRepository, product_id: int, quantity: int) -> Inventory:
+    """ Update inventory """
     check_product(product_id)
     db_inventory = repository.update(Inventory(product_id=product_id, quantity=quantity))
     if db_inventory:
